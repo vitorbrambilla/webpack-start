dde3f57fe46e3bc62ad586ee5ba3244868c3d4cd
vitorbrambilla <vitorbrambilla10@gmail.com>
2021-12-06 13:19:21 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path")
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+              test: /\.js$/,
+              use: ['babel-loader'] 
+            }
+        ]
+    },
+
+    mode: 'development',
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        }
+    }
+}
